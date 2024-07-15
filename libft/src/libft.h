2bc15837257c98e7b952ccad4d52bc0e1026a8b7
arekiu <alex.isaguilera@hotmail.com>
2024-07-15 11:31:17 +0200

    add atoll

--- libft/src/libft.h
+++ libft/src/libft.h
@@ -23,6 +23,7 @@
 # include <limits.h>
 
 int		ft_atoi(const char *str);
+long long	ft_atoll(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 int		ft_isalnum(int num);
